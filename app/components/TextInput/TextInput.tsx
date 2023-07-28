94d5e16f85b40dfc93bccd0354c2c815531ca31f
Azz0102 <nguyenduchuyhht2@gmail.com>
2023-07-28 14:14:10 +0700

    fix error1

--- app/components/TextInput/TextInput.tsx
+++ app/components/TextInput/TextInput.tsx
@@ -1,7 +1,7 @@
 import {useField} from "formik";
 import React from "react";
 
-export const TextInput = ({label, ...props}) => {
+export const TextInput = ({label, ...props}: { label: string, id: string, name: string }) => {
     // useField() returns [formik.getFieldProps(), formik.getFieldMeta()]
     // which we can spread on <input>. We can use field meta to show an error
     // message if the field is invalid and it has been touched (i.e. visited)
