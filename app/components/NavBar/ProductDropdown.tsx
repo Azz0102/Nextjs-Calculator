c161994cc43f5dab7a6ec5d4189e6f89f0003927
Azz0102 <nguyenduchuyhht2@gmail.com>
2023-08-15 10:47:49 +0700

    updatecal

--- app/components/NavBar/ProductDropdown.tsx
+++ app/components/NavBar/ProductDropdown.tsx
@@ -22,7 +22,7 @@ export const ProductDropdown = ({isToggle, setIsToggle}: { isToggle: string, set
                         return '0'
                     }
                 })
-            }} id='SP'>Sản phẩmm
+            }} id='SP'>Sản phẩmmm
                                 <span
                                     className={`after:content-[""]
                                         after:inline-block after:relative 
